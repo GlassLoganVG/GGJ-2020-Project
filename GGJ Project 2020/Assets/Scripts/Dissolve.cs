f481e88c923a35fd20980ec2d602425c6b342a44
Rarrack <44354378+Rarrack@users.noreply.github.com>
2020-02-02 04:39:36 -0500

    Transition test
    Built test for dissolve transition effect.

---
+++ GGJ Project 2020/Assets/Scripts/Dissolve.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    public GameObject transitionStart;
+    public GameObject transitionEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transitionStart.GetComponent<SpriteRenderer>().color = Color.Lerp(transitionStart.GetComponent<SpriteRenderer>().color, new Color(0, 0, 0, 0), Time.deltaTime/3);
+        transitionEnd.GetComponent<SpriteRenderer>().color = Color.Lerp(transitionEnd.GetComponent<SpriteRenderer>().color, new Color(1, 1, 1, 1), Time.deltaTime/3);
+    }
+}
