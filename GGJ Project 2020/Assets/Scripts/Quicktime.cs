3384295d1abff06d9ceb87d7d891e6a96290aa31
Rarrack <44354378+Rarrack@users.noreply.github.com>
2020-02-02 04:01:30 -0500

    Main Gameplay
    Basic functionality of gameplay has been implemented in scene 8 as a test.

---
+++ GGJ Project 2020/Assets/Scripts/Quicktime.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quicktime : MonoBehaviour
+{
+    public Slider quicktimeSlider;
+    bool go = true;
+    bool lower = false;
+    bool raise = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            go = false;
+        }
+
+        if (go)
+        {
+            if (raise == true && lower == false)
+            {
+                quicktimeSlider.value += 10 * Time.deltaTime;
+                if (quicktimeSlider.value == quicktimeSlider.maxValue)
+                {
+                    raise = false;
+                    lower = true;
+                }
+            }
+            if (raise == false && lower == true)
+            {
+                quicktimeSlider.value -= 10 * Time.deltaTime;
+                if (quicktimeSlider.value == quicktimeSlider.minValue)
+                {
+                    lower = false;
+                    raise = true;
+                }
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        raise = false;
+        lower = false;
+    }
+}
